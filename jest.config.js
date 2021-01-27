149f9a4353dd38f9ee4b25ee11faae8124991785
andrecio <andreciobezerra@gmail.com>
2021-01-27 18:39:03 -0300

    add tests setup

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { resolve } = require("path")
+
+module.exports = {
+  rootDir: resolve(__dirname),
+  displayName: "root-tests",
+  testMatch: ["<rootDir/src/**/*.test.ts"],
+  testEnvironment: "node",
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: ["json", "text", "lcov", "clover"],
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "@src/(.*)": "<rootDir>/src/$1",
+    "@test/(.*)": "<rootDir>/test/$1",
+  }
+}
